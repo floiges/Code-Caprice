2489b2c50685c4ef8037eb1931962e40877e30f9
yadong <floiges@163.com>
2024-07-12 11:25:46 +0800

    feat：15 三数之和

---
+++ HashTable/15 Three Sum/main.cpp
@@ -0,0 +1,137 @@
+//
+//  main.cpp
+//  15 Three Sum
+//
+//  Created by 刘亚东 on 2024/7/12.
+//
+
+// 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。
+
+// 请你返回所有和为 0 且不重复的三元组。
+
+// 注意：答案中不可以包含重复的三元组。
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+// 哈希解法
+// 两层for循环就可以确定 a 和b 的数值了，可以使用哈希法来确定 0-(a+b) 是否在 数组里出现过，其实这个思路是正确的，但是我们有一个非常棘手的问题，就是题目中说的不可以包含重复的三元组。
+// 把符合条件的三元组放进vector中，然后再去重，这样是非常费时的，很容易超时
+
+class Solution {
+public:
+    static vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        
+        // 找出 a + b + c = 0
+        // a = nums[i], b = nums[j], c = -(a + b)
+        for (int i = 0; i < nums.size(); i++) {
+            // 排序之后 如果第一个元素已经大于0，那么不可能凑成三元组
+            if (nums[i] > 0) {
+                break;
+            }
+            
+            if (i > 0 && nums[i] == nums[i - 1]) { // 三元组元素 a 去重
+                continue;
+            }
+            
+            unordered_set<int> set;
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (j > i + 2 && nums[j] == nums[j - 1] && nums[j - 1] == nums[j - 2]) {
+                    // 三元组元素 b 去重
+                    continue;
+                }
+                int c = 0 - (nums[i] + nums[j]);
+                if (set.find(c) != set.end()) {
+                    result.push_back({ nums[i], nums[j], c});
+                    set.erase(c); // 三元组元素c去重
+                } else {
+                    set.insert(nums[j]);
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
+// 双指针法
+class Solution2 {
+public:
+    static vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        
+        // 找出 a + b + c = 0;
+        // a = nums[i], b = nums[left], c = nums[right]
+        for (int i = 0; i < nums.size(); i++) {
+            // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
+            if (nums[i] > 0) {
+                return result;
+            }
+            
+            // 错误去重 a 方法，将会漏掉-1,-1,2 这种情况
+            // 我们要做的是 不能有重复的三元组，但三元组内的元素是可以重复的！
+            /*
+            if (nums[i] == nums[i + 1]) {
+                continue;
+            }
+            */
+            // 正确去重 a 方法
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = static_cast<int>(nums.size() - 1);
+            while (right > left) {
+                // 去重复逻辑如果放在这里，0，0，0 的情况，可能直接导致 right<=left 了，从而漏掉了 0,0,0 这种三元组
+                /*
+                while (right > left && nums[right] == nums[right - 1]) right--;
+                while (right > left && nums[left] == nums[left + 1]) left++;
+                */
+                if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                } else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                } else {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    // 去重逻辑应该放在找到一个三元组之后，对 b 和 c 去重
+                    while (right > left && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    while (right > left && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    
+                    // 找到答案时，双指针同时收缩
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+//    vector<int> v1 = {-1, 0, 1, 2, -1, -4};
+//    vector<int> v1 = {0, 1, 1};
+//    vector<int> v1 = {0, 0 ,0};
+    vector<int> v1 = {-2, 0, 1, 1, 2};
+    vector<vector<int>> res = Solution2::threeSum(v1);
+    
+    // 使用范围基的 for 循环打印二维 vector
+    for (const auto& row : res) {
+        for (const auto& value : row) {
+            std::cout << value << " ";
+        }
+        std::cout << std::endl; // 每行后换行
+    }
+    return 0;
+}
